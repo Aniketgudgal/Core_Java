addb5b8b834b5e919dd61ac6050c39905f69d64c
Aniketgudgal <aniketgudgal5867@gmail.com>
2025-10-06 19:16:54 +0530

    Pascal-inspired Triangle with stars

---
+++ Patterns/Pattern6.java
@@ -0,0 +1,22 @@
+// Pascal-Inspired Triangle with Stars
+public class Pattern6
+{
+	public static void main(String x[])
+	{
+		for(int i = 1; i <= 9; i++)
+		{
+			for(int j = 1; j <= 17; j++)
+			{
+				if((i == 9) || (j == 10 - i) || (j == 8+i))
+				{
+					System.out.print("*");
+				}
+				else
+				{
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+	}
+}
