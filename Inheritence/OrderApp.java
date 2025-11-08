be5f1cc8fedc68da8251d956752a6572caadf7ac
Aniketgudgal <aniketgudgal5867@gmail.com>
2025-11-08 19:29:00 +0530

    Devlivery charges of different mode using method overriding

---
+++ Inheritence/OrderApp.java
@@ -0,0 +1,57 @@
+/*
+6. Question:
+ Create a base class Order with fields orderId and amount.
+Subclass CODOrder adds a fixed delivery charge of ₹50.
+
+
+Subclass OnlinePaymentOrder adds no delivery charge but gives 5% cashback.
+ Write a program to calculate the final bill amount using overridden methods.
+
+
+Explanation:
+ Tests overriding with additional logic and conditional calculations in subclasses.
+*/
+class Order
+{
+	int orderId;
+	int amount;
+	int bill()
+	{
+		return 0;
+	}
+	void finalBill()
+	{
+		System.out.println("Order id: "+orderId + " Order Amount: "+amount+" Devliery charges: "+bill());
+		System.out.println("Total Bill is: "+(amount+bill()));
+	}
+}
+class CODOrder extends Order
+{
+	int bill()
+	{
+		return 50;
+	}
+}
+class OnlinePaymentOrder extends Order
+{
+	int bill()
+	{
+		return 0;
+	}
+}
+class OrderApp
+{
+	public static void main(String x[])
+	{
+		OnlinePaymentOrder o1 = new OnlinePaymentOrder();
+		o1.orderId = 1;
+		o1.amount = 500;
+		
+		CODOrder c1 = new CODOrder();
+		c1.orderId = 1;
+		c1.amount = 500;
+		
+		o1.finalBill();
+		c1.finalBill();
+	}
+}
