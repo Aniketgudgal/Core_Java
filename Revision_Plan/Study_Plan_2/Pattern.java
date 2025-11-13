da82782773735256e1022e8999e30207e7109e1d
Aniketgudgal <aniketgudgal5867@gmail.com>
2025-11-13 21:52:24 +0530

    Loose Coupling App

---
+++ Revision_Plan/Study_Plan_2/Pattern.java
@@ -0,0 +1,24 @@
+public class Pattern
+{
+	public static void main(String x[])
+	{
+		for(int i = 1; i <= 7; i++)
+		{
+			for(int j = 1; j <= 7; j++)
+			{
+				if(j <= i)
+				{
+					if(j % 2 == 0)
+					{
+						System.out.print(" *");
+					}
+					else
+					{
+						System.out.print(" "+i);
+					}
+				}
+			}
+			System.out.println();
+		}
+	}
+}
