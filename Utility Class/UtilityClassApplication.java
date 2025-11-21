2f7d069029830786966efca7c4d4236ff504f579
Aniketgudgal <aniketgudgal5867@gmail.com>
2025-11-22 00:55:12 +0530

    Utility class in java by using constructor

---
+++ Utility Class/UtilityClassApplication.java
@@ -0,0 +1,23 @@
+/*
+Utility class means class cannot create its object and it contains only static methods and utility class normally used for work as helper class.
+How to create a utility class ?
+If we want to create a utility class we must declare the class constructor as private and define only static methods in it 
+*/
+class UtilityClass
+{
+	private UtilityClass()
+	{
+		
+	}
+	static void utilityMethod()
+	{
+		System.out.println("This method show the utility class example");
+	}
+}
+public class UtilityClassApplication
+{
+	public static void main(String x[])
+	{
+		UtilityClass.utilityMethod();
+	}
+}
