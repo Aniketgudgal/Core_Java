a840ca703a0093b6d2c38d6c00f9485b99c4c679
Aniketgudgal <aniketgudgal5867@gmail.com>
2025-11-24 22:53:10 +0530

    Check if Array is sorted or not

--- Revision_Plan/Arrays/IsSortedArray.java
+++ Revision_Plan/Arrays/IsSortedArray.java
@@ -12,18 +12,28 @@ public class IsSortedArray
 	public static void main(String x[])
 	{
 		Scanner sc = new Scanner(System.in);
-		int[] num = {1,2,2,3,4};
-		int j = 0;
+		System.out.print("Enter the size of Array: ");
+		int size = sc.nextInt();
+		int[] num = new int[size];
+		// enter the values in array
+		System.out.print("Enter the values in array: ");
+		for(int i = 0; i < num.length; i++)
+		{
+			num[i] = sc.nextInt();
+		}
+		// logic to check the array is sorted or not
+		int j = 0; // using the two pointer to check the array is sorted or not
 		boolean flag = true;
 		for(int i = 0; i < num.length - 1; i++)
 		{
 			 j = i+1;
 			 if(num[i] > num[j])
 			 {
-				flag = false;
-				break;
+				flag = false;  // change the flag variable values
+				break; // if the array is not sorted then break the loop
 			 }
 		}
+		// display the output
 		if(flag)
 		{
 			System.out.println("The Array is Sorted");
