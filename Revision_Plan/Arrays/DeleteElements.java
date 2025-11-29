c1d1284d6081e99d3dcd08ba930af4803c47326a
Aniketgudgal <aniketgudgal5867@gmail.com>
2025-11-29 15:04:36 +0530

    Deleting element from array

--- Revision_Plan/Arrays/DeleteElements.java
+++ Revision_Plan/Arrays/DeleteElements.java
@@ -10,8 +10,17 @@ public class DeleteElements
 	public static void main(String x[])
 	{
 		Scanner sc = new Scanner(System.in);
-		int nums[] = {1,2,3,4,5};
-		int k = 3;
+		System.out.print("Enter the size of array: ");
+		int size = sc.nextInt();
+		int nums[] = new int[size];
+		System.out.print("Enter the values in array: ");
+		for(int i = 0; i < nums.length; i++)
+		{
+			nums[i] = sc.nextInt();
+		}
+		// take the index of value which we want to delete
+		System.out.print("Enter the index value to delete: ");
+		int k = sc.nextInt();
 		if(k >= 0 && k < nums.length - 1)
 		{
 			while(k < nums.length - 1)
@@ -21,15 +30,14 @@ public class DeleteElements
 				nums[k] = nums[k] - nums[k+1];
 				k++;
 			}
+			for(int i = 0; i < nums.length - 1; i++)
+			{
+				System.out.print(" "+nums[i]);
+			}
 		}
 		else
 		{
 			System.out.println("The index value is not present");
-		}
-		// print array
-		for(int i = 0; i < nums.length - 1; i++)
-		{
-			System.out.print(" "+nums[i]);
 		}
 	}
 }
