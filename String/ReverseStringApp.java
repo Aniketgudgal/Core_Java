f358c60b925adc22c0bc757541e6c479ca159bf1
Aniketgudgal <aniketgudgal5867@gmail.com>
2026-01-10 10:58:10 +0530

    String reverse using user defined utility class

---
+++ String/ReverseStringApp.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class ReverseStringApp
+{
+	public static void main(String x[])
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the string input: ");
+		String in = sc.nextLine();
+		String result = ReverseString.reverseString(in);
+		System.out.println("Reverse String is: "+result);
+	}
+}
+class ReverseString
+{
+	// utility class
+	private ReverseString(){};
+	public static String reverseString(String s)
+	{
+		String sb = "";
+		for(int i = s.length() - 1; i >= 0 ; i--)
+		{
+			sb = sb + s.charAt(i);
+		}
+		return sb;
+	}
+}
