8d910fa65f648d74434e188e68cedeecffa81ed3
Aniketgudgal <aniketgudgal5867@gmail.com>
2026-01-28 22:07:15 +0530

    first repeated character in string

---
+++ String/RepeatedCharacterCollection.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+public class RepeatedCharacterCollection
+{
+	public static void main(String x[])
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the string input: ");
+		String in = sc.nextLine();
+		ArrayList<Character> al = new ArrayList<>();
+		for(int i = 0; i < in.length(); i++)
+		{
+			if(al.contains(in.charAt(i)))
+			{
+				System.out.println("First character is: "+in.charAt(i));
+				break;
+			}
+			al.add(in.charAt(i));
+		}
+	}
+}
