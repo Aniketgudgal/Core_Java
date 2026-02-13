565dc461309ddf7901e1e960150124abf1a3a96b
Aniketgudgal <aniketgudgal5867@gmail.com>
2026-02-14 01:07:52 +0530

    unique character in a string

---
+++ Revision_Plan/Study_Plan_4/arrays/UniqueCharacterString.java
@@ -0,0 +1,44 @@
+/*
+First Unique Character in a String
+Given a string s, return the first non-repeating character.
+If none exists, return _.
+Example
+Input: "leetcode" → "l"
+Input: "aabb" → "_"
+*/
+import java.util.Scanner;
+public class UniqueCharacterString
+{
+	public static void main(String x[])
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the string input: ");
+		String str = sc.next();
+		String result = "_";
+		for(int i = 0; i < str.length() - 1; i++)
+		{
+			int count = 0;
+			for(int j = i+1; j < str.length(); j++)
+			{
+				if(str.charAt(i) == str.charAt(j))
+				{
+					count++;
+				}
+			}
+			boolean flag = true;
+			for(int k = 0; k < i; k++)
+			{
+				if(str.charAt(i) == str.charAt(k))
+				{
+					flag = false;
+				}
+			}
+			if(flag && count < 1)
+			{
+				result = str.charAt(i)+"";
+				break;
+			}
+		}
+		System.out.println("The result is: "+result);
+	}
+}
