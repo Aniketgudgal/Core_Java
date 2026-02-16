56a36649698d15b17383292f8d5df45413309a19
Aniketgudgal <aniketgudgal5867@gmail.com>
2026-02-16 12:19:29 +0530

    Full Pyramid

---
+++ Revision_Plan/Study_Plan_4/Patterns/FullPyramid.java
@@ -0,0 +1,24 @@
+public class FullPyramid
+{
+	public static void main(String x[])
+	{
+		for(int i = 1; i <=6; i++)
+		{
+			boolean flag = true;
+			for(int j = 1; j <= 11; j++)
+			{	
+				if(j <= 5+i && j >= 7 - i && flag)
+				{
+					System.out.print("*");
+					flag = false;
+				}
+				else
+				{
+					System.out.print(" ");
+					flag = true;
+				}
+			}
+			System.out.println();
+		}
+	}
+}
