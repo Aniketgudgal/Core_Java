f4ccb5bb3777686838aedd2f9a0a51e5f5d38862
Aniketgudgal <aniketgudgal5867@gmail.com>
2026-03-26 12:51:59 +0530

    Addition of number using command line argument

---
+++ Revision_Plan/Study_Plan_5/Basic Program/AdditionNumCommandLine.java
@@ -0,0 +1,13 @@
+/*
+take command line input and add two number
+*/
+public class AdditionNumCommandLine
+{
+	public static void main(String x[])
+	{
+		int a = Integer.parseInt(x[0]);
+		int b = Integer.parseInt(x[1]);
+		int c = a+b;
+		System.out.println("Addition is: "+c);
+	}
+}
