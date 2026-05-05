cebf0027cc7b558994e05eeddc96d95e84593873
Aniketgudgal <aniketgudgal5867@gmail.com>
2026-05-05 11:42:13 +0530

    Incrypt given String

---
+++ String/IncryptString.java
@@ -0,0 +1,29 @@
+/*
+WAP to input string and incrypt like as?
+Example: abcmnoabc
+Output: a2b2c2m1n1c1
+*/
+import java.util.Scanner;
+public class IncryptString
+{
+	public static void main(String x[])
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the string value: ");
+		String val = sc.next();
+		int[] arr = new int[26];
+		for(int i = 0; i < val.length(); i++)
+		{
+			arr[val.charAt(i) - 'a']++;
+		}
+		StringBuffer result = new StringBuffer();
+		for(int i = 0; i < val.length(); i++)
+		{
+			if(result.indexOf(val.charAt(i)+"") == -1)
+			{
+				result.append(val.charAt(i)+""+arr[val.charAt(i)-'a']);
+			}
+		}
+		System.out.println("Incrypted String is: "+result);
+	}
+}
