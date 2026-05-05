c55b0e91cffcb0384ec8f938c751e9f50666d86c
Aniketgudgal <aniketgudgal5867@gmail.com>
2026-05-05 11:33:23 +0530

    Remove the white spaces from string

---
+++ String/RemoveWhiteSpace.java
@@ -0,0 +1,28 @@
+/*
+WAP to input string and remove the white spaces from a string?
+*/
+import java.util.Scanner;
+public class RemoveWhiteSpace
+{
+	public static void main(String x[])
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the String value: ");
+		String val = sc.nextLine();
+		String result = removeSpace(val);
+		System.out.println("Result String is: "+result);
+	}
+	public static String removeSpace(String s)
+	{
+		StringBuffer sb = new StringBuffer();
+		for(int i = 0; i < s.length(); i++)
+		{
+			if(s.charAt(i)== ' ')
+			{
+				continue;
+			}
+			sb.append(s.charAt(i));
+		}
+		return sb.toString();
+	}
+}
