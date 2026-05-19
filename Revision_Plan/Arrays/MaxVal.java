862c44947a1790e73838dde1ebfa24b68e61fbc8
Aniketgudgal <aniketgudgal5867@gmail.com>
2026-05-19 16:00:13 +0530

    Max value from arrayg

---
+++ Revision_Plan/Arrays/MaxVal.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class MaxVal
+{
+	public static void main(String x[])
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the size of Array: ");
+		int size = sc.nextInt();
+		int[] arr = new int[size];
+		System.out.print("Enter the values in array: ");
+		for(int i = 0; i < arr.length; i++)
+		{
+			arr[i] = sc.nextInt();
+		}
+		int max = Integer.MIN_VALUE;
+		for(int i = 0; i < arr.length; i++)
+		{
+			if(arr[i] > max)
+			{
+				max = arr[i];
+			}
+		}
+		System.out.println("Max Value is: "+max);
+	}
+}
