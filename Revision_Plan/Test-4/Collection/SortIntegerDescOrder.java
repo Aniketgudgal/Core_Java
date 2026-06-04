58427c78416a632594003a371ec14c9d40a9c1ba
Aniketgudgal <aniketgudgal5867@gmail.com>
2026-06-04 12:27:31 +0530

    Sort Data in Descending order by using Comparator interface

---
+++ Revision_Plan/Test-4/Collection/SortIntegerDescOrder.java
@@ -0,0 +1,72 @@
+/*
+Sort Integers in Descending Order
+Description: Arrange integers from largest to smallest.
+*/
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Collections;
+class Student
+{
+	private String name;
+	private int id;
+	private int rollNo;
+	public Student(String name, int id, int rollNo)
+	{
+		this.name = name;
+		this.id = id;
+		this.rollNo = rollNo;
+	}
+	public Student()
+	{
+	}
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	public String getName()
+	{
+		return name;
+	}
+	public void setId(int id)
+	{
+		this.id = id;
+	}
+	public int getId()
+	{
+		return id;
+	}
+	public void setRollNo(int rollNo)
+	{
+		this.rollNo = rollNo;
+	}
+	public int getRollNo()
+	{
+		return rollNo;
+	}
+	public String toString()
+	{
+		return name+", "+id+", "+rollNo;
+	}
+}
+public class SortIntegerDescOrder
+{
+	public static void main(String x[])
+	{
+		ArrayList<Student> al = new ArrayList<>();
+		al.add(new Student("Tahir",3,1400));
+		al.add(new Student("Aniket",1,400));
+		al.add(new Student("Gokul",4,6000));
+		al.add(new Student("Omkar",2,7000));
+		System.out.println("Before Sorting");
+		for(Student s: al)
+		{
+			System.out.println(s);
+		}
+		Collections.sort(al,(s1,s2) -> s1.getId()< s2.getId() ? 1: s1.getId() > s2.getId() ? -1 : 0);
+		System.out.println("After sorting");
+		for(Student s: al)
+		{
+			System.out.println(s);
+		}
+	}
+}
