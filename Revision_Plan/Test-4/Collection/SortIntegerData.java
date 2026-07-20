4deb873094bd12baa6af497b3ed798e259783a3c
Aniketgudgal <aniketgudgal5867@gmail.com>
2026-07-20 21:38:41 +0530

    Updated code

--- Revision_Plan/Test-4/Collection/SortIntegerData.java
+++ Revision_Plan/Test-4/Collection/SortIntegerData.java
@@ -5,6 +5,7 @@ Description: Arrange integers from smallest to largest.
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+// POJO class
 class Employee
 {
 	private String name;
